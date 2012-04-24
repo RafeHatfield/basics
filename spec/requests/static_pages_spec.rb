f65d8ff67ac7acf6b3b55cdfded35cf88711f924
Rafe Hatfield <rafe.hatfield@gmail.com>
2012-04-24 07:04:55 -0700

    tidied up specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,7 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_selector('h1', :text => 'Sample App') }
-    it { should have_selector('title', :text => "#{base_title}") }
+    it { should have_selector('title', :text => full_title('')) }
     it { should_not have_selector('title', :text => '| Home') }
   end
 
@@ -19,7 +19,7 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => "#{base_title} | Help") }
+    it { should have_selector('title', :text => full_title('Help')) }
   end
 
   describe "About page" do
@@ -27,7 +27,7 @@ describe "Static pages" do
     before { visit about_path }
 
     it { should have_selector('h1', :text => 'About Us') }
-    it { should have_selector('title', :text => "#{base_title} | About Us") }
+    it { should have_selector('title', :text => full_title('About Us')) }
   end
   
   describe "Contact page" do
@@ -35,6 +35,6 @@ describe "Static pages" do
     before { visit contact_path }
 
     it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact") }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
