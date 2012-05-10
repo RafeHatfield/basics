a84cc125ddb54a85e73891b316a570c86e2ae565
Rafe Hatfield <rafe.hatfield@gmail.com>
2012-05-09 21:37:47 -0700

    user session management

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -57,6 +57,12 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+        
+        describe "followed by signout" do
+          before { click_link 'Sign out' }
+          it { should have_link('Sign in') }
+        end
       end
     end
   end
